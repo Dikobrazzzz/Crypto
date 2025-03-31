d3f9de64a1547c08ac52f887e207390345ef66c4
Dikobrazzzz <roman@MacBook-Air-Roman.local>
2025-03-31 21:48:51 +0300

    jaeger

--- internal/usecase/wallet.go
+++ internal/usecase/wallet.go
@@ -4,34 +4,44 @@ import (
 	"context"
 	"crypto/internal/models"
 	"crypto/internal/repository"
+
+	"go.opentelemetry.io/otel"
 )
 
 type WalletUsecase struct {
-	WalletCache repository.WalletProvider
+	WalletRepo repository.WalletProvider
 }
 
 func NewWalletProvider(cache repository.WalletProvider) *WalletUsecase {
 	return &WalletUsecase{
-		WalletCache: cache,
+		WalletRepo: cache,
 	}
 }
 
 func (w *WalletUsecase) CreateAddress(ctx context.Context, req *models.AddressRequest) (*models.Address, error) {
-
-	return w.WalletCache.CreateAddress(ctx, req)
+	tracer := otel.Tracer("Crypto-api")
+	ctx, span := tracer.Start(ctx, "CreateAddress")
+	defer span.End()
+	return w.WalletRepo.CreateAddress(ctx, req)
 }
 
 func (w *WalletUsecase) GetID(ctx context.Context, id uint64) (*models.Address, error) {
-
-	return w.WalletCache.GetID(ctx, id)
+	tracer := otel.Tracer("Crypto-api")
+	ctx, span := tracer.Start(ctx, "GetID")
+	defer span.End()
+	return w.WalletRepo.GetID(ctx, id)
 }
 
 func (w *WalletUsecase) GetAllWallets(ctx context.Context) ([]models.Address, error) {
-
-	return w.WalletCache.GetAllWallets(ctx)
+	tracer := otel.Tracer("Crypto-api")
+	ctx, span := tracer.Start(ctx, "GetAllWallets")
+	defer span.End()
+	return w.WalletRepo.GetAllWallets(ctx)
 }
 
 func (w *WalletUsecase) EditTag(ctx context.Context, req *models.TagUpdateRequest) error {
-
-	return w.WalletCache.EditTag(ctx, req)
+	tracer := otel.Tracer("Crypto-api")
+	ctx, span := tracer.Start(ctx, "EditTag")
+	defer span.End()
+	return w.WalletRepo.EditTag(ctx, req)
 }
--- internal/usecase/wallet_integration_test.go
+++ internal/usecase/wallet_integration_test.go
@@ -1,13 +1,12 @@
-package usecase_test
+package usecase
 
 import (
 	"context"
+	"crypto/config"
 	"crypto/internal/models"
 	"crypto/internal/repository"
-	"crypto/internal/usecase"
-	"database/sql"
-	"log"
-	"os"
+	"crypto/internal/storage"
+	"log/slog"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -15,62 +14,79 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var db *sql.DB
-
-func TestMain(m *testing.M) {
-	dsn := "postgres://postgres:postgres@127.0.0.1:5432/test_db?sslmode=disable"
-
-	var err error
-	db, err = sql.Open("postgres", dsn)
-	if err != nil {
-		log.Fatalf("failed to connect to DB: %v", err)
-	}
-
-	if err = db.Ping(); err != nil {
-		log.Fatalf("failed to ping DB: %v", err)
-	}
-
-	if err := createTables(); err != nil {
-		log.Fatalf("failed to create tables: %v", err)
-	}
-
-	code := m.Run()
-
-	_ = db.Close()
-	os.Exit(code)
-}
-
-func createTables() error {
-	_, err := db.Exec(`
-		CREATE TABLE IF NOT EXISTS addresses (
-			id SERIAL PRIMARY KEY,
-			wallet_address TEXT NOT NULL,
-			chain_name TEXT NOT NULL,
-			crypto_name TEXT NOT NULL,
-			tag TEXT
-		)
-	`)
-	return err
-}
-
 func TestWalletUsecase_CreateAddress(t *testing.T) {
-	repo := repository.NewPostgresWalletRepo(db)
-	walletUsecase := usecase.NewWalletProvider(repo)
 
 	ctx := context.Background()
-	req := &models.AddressRequest{
-		WalletAddress: "0xABC",
-		ChainName:     "Ethereum",
-		CryptoName:    "ETH",
-		Tag:           "myTag",
+	pool, err := storage.GetConnection(ctx, config.AppConfig.DatabaseURL)
+	if err != nil {
+		slog.Error("Error getting connection", "error", err)
+		return
+	}
+	defer pool.Close()
+
+	repo := repository.NewPostgresWalletRepo(pool)
+	walletUsecase := NewWalletProvider(repo)
+
+	testCases := []struct {
+		name          string
+		WalletAddress string
+		ChainName     string
+		CryptoName    string
+		Tag           string
+		expectError   bool
+	}{
+		{
+			name:          "success call",
+			WalletAddress: "9999999999999999999999999FFFFFFF",
+			ChainName:     "Ethereum",
+			CryptoName:    "ETH",
+			Tag:           "myTag",
+			expectError:   false,
+		},
+		{
+			name:          "success call",
+			WalletAddress: "",
+			ChainName:     "Ethereum",
+			CryptoName:    "ETH",
+			Tag:           "myTag",
+			expectError:   false,
+		},
+		{
+			name:          "success call",
+			WalletAddress: "0xABCASDFIO",
+			ChainName:     "X",
+			CryptoName:    "ETH",
+			Tag:           "5",
+			expectError:   false,
+		},
+		{
+			name:          "success call",
+			WalletAddress: "0xABC",
+			ChainName:     "Ethereum",
+			CryptoName:    "ETH",
+			Tag:           "",
+			expectError:   false,
+		},
 	}
 
-	address, err := walletUsecase.CreateAddress(ctx, req)
-	require.NoError(t, err)
-	require.NotNil(t, address)
-	require.NotZero(t, address.ID)
-	require.Equal(t, req.WalletAddress, address.WalletAddress)
-	require.Equal(t, req.ChainName, address.ChainName)
-	require.Equal(t, req.CryptoName, address.CryptoName)
-	require.Equal(t, req.Tag, address.Tag)
+	for _, tc := range testCases {
+		req := &models.AddressRequest{
+			WalletAddress: tc.WalletAddress,
+			ChainName:     tc.ChainName,
+			CryptoName:    tc.CryptoName,
+			Tag:           tc.Tag,
+		}
+		address, err := walletUsecase.CreateAddress(ctx, req)
+		if tc.expectError {
+			require.Error(t, err)
+			return
+		}
+		require.NoError(t, err)
+		require.NotNil(t, address)
+		// require.NotZero(t, address.ID)
+		require.Equal(t, tc.WalletAddress, address.WalletAddress)
+		require.Equal(t, tc.ChainName, address.ChainName)
+		require.Equal(t, tc.CryptoName, address.CryptoName)
+		require.Equal(t, tc.Tag, address.Tag)
+	}
 }
